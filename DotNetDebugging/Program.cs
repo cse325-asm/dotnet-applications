bb4d34534af07b88a94e4acd678fc7ee934b680a
angelasmilford <angela.milford123@gmail.com>
2025-09-23 03:12:12 -1100

    Wrote to the debug console

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,8 +1,13 @@
-﻿int result = Fibonacci(5); // 5 is the argument
+﻿using System.Diagnostics;
+
+int result = Fibonacci(5); // 5 is the argument
 Console.WriteLine(result);
 
 static int Fibonacci(int n) // n is the parameter
 {
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+
     int n1 = 0;
     int n2 = 1;
     int sum;
@@ -12,6 +17,7 @@ static int Fibonacci(int n) // n is the parameter
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
+        Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");    
     }
 
     return n == 0 ? n1 : n2; // Ternary operator: if "n" is 0, return "n1", otherwise return "n2"
