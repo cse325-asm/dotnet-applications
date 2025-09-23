acd4b1d06d9df2b00b812f4196289f436712da74
angelasmilford <angela.milford123@gmail.com>
2025-09-22 15:56:02 -1100

    Added a NuGet package by using the .NET Core tool

--- DotNetDependencies/obj/Debug/net9.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net9.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad8ce3a52439a2d333ff21a9fd90433b1381696")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
