1b40992d553383f71937e8942e0f34a673d6a577
angelasmilford <angela.milford123@gmail.com>
2025-09-23 22:10:51 -1100

    Added another record to the "Create a web API with ASP.NET Core controllers" module in the "Pizzas" List.

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -11,7 +11,8 @@ public static class PizzaService
         Pizzas = new List<Pizza>
         {
             new Pizza { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
-            new Pizza { Id = 2, Name = "Veggie", IsGlutenFree = true }
+            new Pizza { Id = 2, Name = "Veggie", IsGlutenFree = true },
+            new Pizza { Id = 3, Name = "Supreme", IsGlutenFree = true } // My additional record
         };
     }
 
--- ContosoPizza/obj/Debug/net9.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net9.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364721b40a38a2db57bb8e7179f0471ef611c3ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1da25f174233bb9630b4e7127c7a007f402a99")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
